9773b06dcd80b432dc510845219566341c3463d3
Youngseoki <dudtjrdl1492@gmail.com>
2022-11-14 11:23:21 +0900

    entity

---
+++ server/server/src/main/java/com/mainproject/server/auditable/Auditable.java
@@ -0,0 +1,26 @@
+package com.mainproject.server.auditable;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.cglib.core.Local;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class Auditable {
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime modifiedAt = LocalDateTime.now();
+
+}
