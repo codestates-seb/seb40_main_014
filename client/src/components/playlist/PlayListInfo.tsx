afae8cf077cdbb160c0d03fd639f83f38c601c42
devjindev <hyjinny00@gmail.com>
2022-11-30 14:03:42 +0900

    ♻️Refactor: [플리 상세] Info 반응형 갈아엎음

--- client/src/components/playlist/PlayListInfo.tsx
+++ client/src/components/playlist/PlayListInfo.tsx
@@ -49,14 +49,18 @@ const PlayListInfo = ({
 					</div>
 					<div className="options">
 						<div>
-							<img src={picture} alt={playListInfo.name} />
-							<button
-								onClick={() => navigate(`/mypage/${playListInfo.memberId}`)}>
-								{playListInfo.name}
-							</button>
-							<Like {...likeBookmarkProps} />
-							<span>{playListInfo.like}</span>
-							<BookMark {...likeBookmarkProps} />
+							<div>
+								<img src={picture} alt={playListInfo.name} />
+								<button
+									onClick={() => navigate(`/mypage/${playListInfo.memberId}`)}>
+									{playListInfo.name}
+								</button>
+							</div>
+							<div>
+								<Like {...likeBookmarkProps} />
+								<span>{playListInfo.like}</span>
+								<BookMark {...likeBookmarkProps} />
+							</div>
 						</div>
 						<div>
 							{loginId === playListInfo.memberId && (
@@ -81,6 +85,7 @@ const PlayListInfoStyle = styled.div`
 	position: relative;
 	display: flex;
 	flex-direction: column;
+
 	.info {
 		display: flex;
 		justify-content: center;
@@ -88,6 +93,7 @@ const PlayListInfoStyle = styled.div`
 			flex-direction: column;
 		}
 	}
+
 	.total {
 		font-size: ${(props) => props.theme.fontSize.large};
 		margin: 40px 0 25px 0;
@@ -101,25 +107,28 @@ const PlayListInfoStyle = styled.div`
 
 const Img = styled.span`
 	width: 30%;
-
-	img {
-		width: 100%;
-		object-fit: cover;
-	}
-
-	@media (max-width: 850px) {
+	@media (max-width: 980px) {
 		width: 40%;
 	}
 	@media (max-width: 550px) {
 		width: 100%;
 		margin-bottom: 10px;
 	}
+
+	img {
+		width: 100%;
+		object-fit: cover;
+	}
 `;
 
 const Info = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: space-around;
+
 	width: 40%;
 	margin-left: 5%;
-	@media (max-width: 850px) {
+	@media (max-width: 980px) {
 		width: 52%;
 		margin-left: 8%;
 	}
@@ -127,21 +136,15 @@ const Info = styled.div`
 		width: 100%;
 		margin-left: 0;
 	}
-	display: flex;
-	flex-direction: column;
-	justify-content: space-around;
-	div {
-		font-size: ${(props) => props.theme.fontSize.medium};
-	}
+
 	.title {
 		font-size: 22px;
-		/* font-size: ${(props) => props.theme.fontSize.large}; */
 		line-height: 1.5;
 		font-weight: 600;
-
 		.categoryBox {
 			margin: 10px 0;
 		}
+
 		// Tablet
 		@media screen and (max-width: 980px) {
 			font-size: 18px;
@@ -151,35 +154,55 @@ const Info = styled.div`
 			font-size: 18px;
 		}
 	}
-	.options {
-		display: flex;
-		justify-content: space-between;
-		align-items: center;
 
+	.options {
 		> div:first-of-type {
 			display: flex;
-			align-items: center;
-			img {
-				width: 25px;
-				margin: 0 5px;
-				border-radius: 50%;
-				object-fit: cover;
+			flex-direction: column;
+			> div {
+				display: flex;
+				align-items: center;
 			}
-			span {
-				margin-left: 5px;
-				margin-right: 20px;
-			}
-			button {
-				margin-left: 5px;
-				margin-right: 20px;
-				:hover {
-					color: ${(props) => props.theme.colors.gray700};
+			> div:first-of-type {
+				img {
+					width: 25px;
+					margin: 0 5px;
+					border-radius: 50%;
+					object-fit: cover;
+				}
+				button {
+					margin-left: 5px;
+					margin-right: 20px;
+					:hover {
+						color: ${(props) => props.theme.colors.gray700};
+					}
 				}
 			}
-			// Mobile
-			@media screen and (max-width: 640px) {
-				font-size: 14px !important;
+			> div:last-of-type {
+				justify-content: flex-end;
+				margin-top: 10px;
+
+				span {
+					margin-left: 5px;
+				}
+
+				> *:last-child {
+					margin-left: 15px;
+				}
+
+				// Mobile
+				@media screen and (max-width: 640px) {
+					span {
+						font-size: 14px;
+					}
+				}
 			}
+		}
+
+		> div:last-of-type {
+			display: flex;
+			justify-content: flex-end;
+			margin-top: 10px;
 		}
 	}
 `;
