f1de81a3e6ee93338bc8ab3edb6ddd9634d09b05
lielocks <kari300084@gmail.com>
2022-12-01 18:03:00 +0900

    [Chore]

--- server/server/src/main/java/com/mainproject/server/ChatRoom/mapper/ChatRoomMapper.java
+++ server/server/src/main/java/com/mainproject/server/ChatRoom/mapper/ChatRoomMapper.java
@@ -119,10 +119,9 @@ public class ChatRoomMapper {
         List<ChatRoom> chatRoomList = new ArrayList<>();
         for (Member member1 : member) {
             // 멤버가 가지고 있는 룸을 전부 리스트에 추가
-            for (int i = 0; i < member1.getChatRooms().size(); i++) {
-                chatRoomList.add(member1.getChatRooms().get(i));
+            chatRoomList.add(member1.getChatRooms().get(0));
             }
-        }
+
         // 정렬된 ChatRoom 리스트로 한건씩 Dto 생성
         List<RankResponseChatRoomDto> rankResponseChatRoomDtoList = chatRoomList.stream()
                 .map(chatRoom -> chatRoomRankResponseDtoToChatRoom(chatRoom))
