6963d96767186285257beb91c0f076d674c79fae
bbororo <koogoori54@gmail.com>
2022-12-02 16:32:34 +0900

    :hammer: chore : category관련 사용하지 않는 파일삭제 및 수정

--- server/server/src/main/java/com/mainproject/server/playlist/repository/CategoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.mainproject.server.playlist.repository;
-
-import com.mainproject.server.playlist.entity.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-    Category findByCategory(String category);
-}
