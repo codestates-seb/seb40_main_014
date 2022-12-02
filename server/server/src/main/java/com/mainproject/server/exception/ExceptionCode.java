745aff04d38277791b1c7fdd265a8f20a9fb26c9
Youngseoki <108003862+Youngseoki@users.noreply.github.com>
2022-12-02 11:30:55 +0900

    🐛 fix : ExceptionCode
    🐛 fix : ExceptionCode

--- server/server/src/main/java/com/mainproject/server/exception/ExceptionCode.java
+++ server/server/src/main/java/com/mainproject/server/exception/ExceptionCode.java
@@ -12,7 +12,7 @@ public enum ExceptionCode {
     TOKEN_NOT_AVAILABLE(401, "유효하지 않은 토큰입니다"),
     BAD_REQUEST(400, "잘못된 요청입니다."),
     ROOM_NOT_EXISTS(404, "채팅방이 존재하지 않습니다."),
-    MEMBER_NAME_ALREADY_EXISTS(404, " memberName already exists");
+    EMAIL_ALREADY_EXISTS(404, "중복된 이메일입니다.");
 
     @Getter
     private int status;
