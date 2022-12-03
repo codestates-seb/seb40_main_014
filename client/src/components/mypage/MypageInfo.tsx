f1ac7c96529b7a61cc70819761213cd3430a281e
devjindev <hyjinny00@gmail.com>
2022-12-03 21:36:46 +0900

    ⛏️Chore: 랭킹 덮어쓰기

--- client/src/components/mypage/MypageInfo.tsx
+++ client/src/components/mypage/MypageInfo.tsx
@@ -94,26 +94,26 @@ const MypageInfo = ({ userInfo, myId }: MypageInfoType) => {
 									cursor="pointer"
 									fontSize="14px"
 								/>
-								<InfoText ref={infoTextRef}>
+								<MyInfoText ref={infoTextRef}>
 									<BadgeInfo>
 										<div>
 											<Badge grade="SILVER" />
-											<span>이상</span>
+											<span>팔로워 + 플리 좋아요 0 이상</span>
 										</div>
 										<div>
 											<Badge grade="GOLD" />
-											<span>이상</span>
+											<span>팔로워 + 플리 좋아요 5 이상</span>
 										</div>
 										<div>
 											<Badge grade="VIP" />
-											<span>이상</span>
+											<span>팔로워 + 플리 좋아요 10 이상</span>
 										</div>
 										<div>
 											<Badge grade="LUVIP" />
-											<span>이상</span>
+											<span>팔로워 + 플리 좋아요 20 이상</span>
 										</div>
 									</BadgeInfo>
-								</InfoText>
+								</MyInfoText>
 							</Info>
 						</Title>
 						<div>
@@ -208,6 +208,10 @@ const MyInfo = styled.div`
 		display: flex;
 		margin-bottom: 15px;
 	}
+`;
+
+const MyInfoText = styled(InfoText)`
+	width: 265px;
 `;
 
 const BadgeInfo = styled.div`
